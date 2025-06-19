1599d51324aef102be4cdd643f2e5c664d284600
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-06-19 15:21:37 +0100

    add run_analysis.sh bash script

---
+++ inst/bash/run_analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=FluTracer
+#SBATCH --ntasks=1
+#SBATCH --mem=5GB
+#SBATCH --time=01:00:00
+#SBATCH --output=FluTracer.log
+pwd; hostname; date
+
+echo "Running FluTracer analysis script"
+
+module load R
+
+Rscript inst/scripts/run_analysis.R
+
+date
