fbc9ab17d3e1183758cc82a4f452f68a9515f19c
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-06-23 15:04:14 +0100

    increase RAM in run_analysis.sh to prevent oom error

--- inst/bash/run_analysis.sh
+++ inst/bash/run_analysis.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=FluTracer
 #SBATCH --ntasks=16
 #SBATCH --nodes=1
-#SBATCH --mem=5GB
+#SBATCH --mem=10GB
 #SBATCH --time=01:00:00
 #SBATCH --output=FluTracer.log
 pwd; hostname; date
