c037e4a29cf8a22e0ff3c2558396fa615bdcca56
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-07-02 21:35:30 +0100

    update plot_prop_outbreak_control_reproduction_number script and plot

--- inst/scripts/plot_prop_outbreak_control_reproduction_number.R
+++ inst/scripts/plot_prop_outbreak_control_reproduction_number.R
@@ -2,13 +2,13 @@ library(data.table)
 library(ggplot2)
 
 # read simulation results saved by running inst/scripts/run_analysis.R
-h5n1_results <- readRDS(file.path("inst", "extdata", "h5n1_simulations.rds"))
-h1n1_results <- readRDS(file.path("inst", "extdata", "h1n1_simulations.rds"))
-h7n9_results <- readRDS(file.path("inst", "extdata", "h7n9_simulations.rds"))
+h5n1_results <- readRDS(file.path("inst", "extdata", "H5N1_simulations_no_Q.rds"))
+h1n1_results <- readRDS(file.path("inst", "extdata", "H1N1_simulations_no_Q.rds"))
+h7n9_results <- readRDS(file.path("inst", "extdata", "H7N9_simulations_no_Q.rds"))
 
-h5n1_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 500), by = scenario]
-h1n1_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 500), by = scenario]
-h7n9_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 500), by = scenario]
+h5n1_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 5000), by = scenario]
+h1n1_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 5000), by = scenario]
+h7n9_results[, pext := ringbp::extinct_prob(sims[[1]], cap_cases = 5000), by = scenario]
 
 h5n1_data <- rbindlist(h5n1_results$data)
 h5n1_data[, `:=`(scenario = h5n1_results$scenario, pext = h5n1_results$pext)]
@@ -26,12 +26,12 @@ rm(h7n9_results)
 flu_data <- rbindlist(list(h5n1_data, h1n1_data, h7n9_data))
 
 prop_outbreak_control <- flu_data[
-  num.initial.cases == 20 & theta == "15%" & prop.asym == 0,
-  .(control_effectiveness, index_R0, pext, subtype, delay)
+  initial_cases == 20 & prop_presymptomatic == 0.15 & prop_asymptomatic == 0,
+  .(prop_ascertain, r0_community, pext, subtype, delay)
 ]
 
 # convert to percentages for plotting
-prop_outbreak_control[, control_effectiveness := control_effectiveness * 100]
+prop_outbreak_control[, prop_ascertain := prop_ascertain * 100]
 prop_outbreak_control[, pext := pext * 100]
 
 # factorise and order delay type for plotting order
@@ -48,18 +48,18 @@ delay_labels <- c(
 prop_outbreak_control_plot <- ggplot2::ggplot(data = prop_outbreak_control) +
   ggplot2::geom_line(
     mapping = ggplot2::aes(
-      x = control_effectiveness,
+      x = prop_ascertain,
       y = pext,
-      colour = as.factor(index_R0),
+      colour = as.factor(r0_community),
       linetype = as.factor(subtype)
     ),
     linewidth = 0.75
   ) +
   ggplot2::geom_point(
     mapping = ggplot2::aes(
-      x = control_effectiveness,
+      x = prop_ascertain,
       y = pext,
-      fill = as.factor(index_R0),
+      fill = as.factor(r0_community),
       shape = as.factor(subtype)
     ),
     size = 3,
